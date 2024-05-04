7298e68dda7c12dce57aa327a87a665e0f03f323
zafkiel45 <zafkielc@gmail.com>
2024-05-04 10:24:07 -0300

    feat: added main content and about_me content
    the main content with a button to make a budget and a section about me added

---
+++ portifolio/app/components/main/mainContent.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@nextui-org/button"
+import { noto_sans } from "@/config/fonts"
+
+export const MainContent = () => {
+    return (
+        <main className="px-2 w-full dark:text-[#F1F1F1] h-[80vh] flex flex-col gap-5 items-center justify-center">
+            <div className="text-center text-3xl ">
+                <h1 className={noto_sans.className}>
+                    Desenvolvedor
+                    <br />
+                    Front-end
+                </h1>
+            </div>
+            <div className="text-center">
+            Construindo páginas modernas para
+            <br />
+            você, meu estimado cliente.
+            </div>
+            <div>
+                <Button aria-label="botão para fazer orçamento" size="sm" className="bg-[#1E1E1E] font-semibold active:relative active:-top-2 hover:scale-105 transition-all dark:bg-[#9651AE]">
+                    Fazer orçamento
+                </Button>
+            </div>
+        </main>
+    )
+}
--- portifolio/app/page.tsx
+++ portifolio/app/page.tsx
@@ -1,8 +1,12 @@
 import { Header } from "./components/header/header";
+import { MainContent } from "./components/main/mainContent";
+import { AboutMe } from "./components/about/about_me";
 export default function Home() {
 	return (
 		<>
 			<Header/>
+			<MainContent/>
+			<AboutMe/>
 		</>	
 	);
 }
