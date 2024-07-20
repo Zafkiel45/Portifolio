2513a788972b8162ff14625a56431ba0bc30ba4e
zafkiel45 <zafkielc@gmail.com>
2024-07-20 16:36:19 -0300

    fix: main design improve

--- portifolio/app/components/main/mainContent.tsx
+++ portifolio/app/components/main/mainContent.tsx
@@ -6,7 +6,7 @@ export const MainContent = () => {
   return (
     <main
       id="home2"
-      className="px-2 desktop:px-14 fullhd:gap-10 fullhd:justify-center fullhd:px-20 tablet:px-8 mobile:px-4 w-full h-[90vh] mobile:h-screen flex flex-col gap-5 items-center justify-center"
+      className="px-2 desktop:px-14 fullhd:gap-10 fullhd:justify-center fullhd:px-20 tablet:px-8 mobile:px-4 w-full h-screen flex flex-col gap-7 items-center justify-center"
     >
       <div className="text-center text-3xl fullhd:text-8xl desktop:text-6xl tablet:text-5xl mobile:text-4xl ">
         <h1 className={noto_sans.className}>
@@ -25,7 +25,7 @@ export const MainContent = () => {
           <Button
             aria-label="botão para fazer orçamento"
             size="sm"
-            className="bg-[#1E1E1E] fullhd:text-2xl fullhd:p-7 text-white desktop:text-lg mobile:text-base font-semibold active:relative active:-bottom-1 hover:scale-105 transition-all dark:bg-[#9651AE]"
+            className="bg-[#9651AE] fullhd:text-2xl desktop:px-5 desktop:py-2 fullhd:p-7 text-base text-white desktop:text-lg font-semibold active:relative active:-bottom-1 hover:scale-105 transition-all"
           >
             Contato
           </Button>
